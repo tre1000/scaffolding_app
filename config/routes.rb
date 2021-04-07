5f31312ae44e1c94735540376915004707263726
Tyler Epstein <tylerepstein27@gmail.com>
2021-04-06 23:20:09 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :posts
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # namespace :api do
+  #   get "/photos" => "photos#index"
+  # end
+end
